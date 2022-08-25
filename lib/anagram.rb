94d0c7eb94663c948887b648c9da6004e535262f
gloria oduor <gloriaoduor@gmail.com>
2022-08-25 16:20:15 +0300

    Done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Anagram 
+    attr_reader :word
+    def initialize (word)
+      @word = word 
+    end 
+  
+    def match(array)
+      anagrams = []
+      array.each do |element|
+        if element.split("").sort == @word.split("").sort
+          anagrams << element
+        else 
+          anagrams
+        end 
+      end 
+      anagrams
+    end 
+  
+  end 
